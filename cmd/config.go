129b49b8e0b466204ef93d2ab4a88100157ab832
Alex <alex@serversaurus.com>
2013-09-10 13:30:10 +1000

    and still broken

--- cmd/config.go
+++ cmd/config.go
@@ -86,7 +86,7 @@ func (c configCmd) testCredentials(host string, user string, pass string) error 
 	if err != nil {
 		return err
 	}
-	_, err := client.GetProfile()
+	_, err = client.GetProfile()
 	if err != nil {
 		return err
 	}
