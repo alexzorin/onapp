fe7b30d45326c84252e1246057871583f77bf2d1
Alex <alex@serversaurus.com>
2013-09-12 12:34:47 +1000

    test was broken after removing verbose

--- cmd/config_test.go
+++ cmd/config_test.go
@@ -6,15 +6,12 @@ import (
 
 func TestMergeConfigs(t *testing.T) {
 	c1 := &config{ApiUser: "a", ApiKey: "abcd"}
-	c2 := &config{ApiUser: "b", Verbose: true}
+	c2 := &config{ApiUser: "b"}
 	merged, err := mergeConfigs(c1, c2)
 	if err != nil {
 		t.Error(err)
 	}
 	if merged.ApiUser != "a" {
 		t.Fatal("ApiUser shouldn't have been overriden")
-	}
-	if !merged.Verbose {
-		t.Fatal("Verbose should have been true")
 	}
 }
